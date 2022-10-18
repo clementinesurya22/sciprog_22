dab293f93444fe55970a618444e65d407913f766
Clementine Surya <clementine@Clementines-MacBook-Pro.local>
2022-10-18 16:08:40 +0100

    Codes for Practical 3

---
+++ Practical3/use_printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+  int i1=10;
+  int i2=200;
+  float fl1=1.11;
+  float fl2=2.22;
+
+  printf("Two ints %d %d and floats %.2f %f",i1,i2,fl1,fl2);
+
+
+  return 0; 
+
+}
